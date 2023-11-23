69f4cd141aa15fb897ff84a9c101f1bdbb7cef62
Prayrana Khadye <103586617+pksuperduper@users.noreply.github.com>
2023-11-23 15:02:11 -0500

    Create Pray.js

---
+++ Pray.js
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+	function goMadLib(formData) { 
+		console.log("here");
+    //let formData = document.body.getElementById("formData");
+
+    let noun = formData.elements.namedItem("noun").value;
+    let activity = formData.elements.namedItem("activity").value;
+    let different = formData.elements.namedItem("different").value;
+    let final = formData.elements.namedItem("final").value;
+    
+    let newContent = "I found myself in a " + noun + ".";
+
+    newContent += "I wanted to know what I should do and I did this -- " + activity + ".";
+
+    newContent += "Then, I decided to do something different and " + different + ".";
+    
+    newContent += "Finally, I did this --" + final + ".";
+    
+    document.getElementById("content").innerHTML = newContent;
+    
+	}
+	
